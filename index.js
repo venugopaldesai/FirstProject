3d3df8ed33bcbd36aec2a0cde8a3434d40be178d
venugopaldesai <46933850+venugopaldesai@users.noreply.github.com>
2022-10-08 20:57:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// javascript
+let count=0
+let countEl=document.getElementById("count-el")
+let saveEl=document.getElementById("save-el")
+
+function increment()
+{
+    count+=1
+    countEl.innerText=count
+    
+}
+function save()
+{
+    saveEl.textContent+=count+" - "
+    count=0
+    countEl.innerText=0
+}
